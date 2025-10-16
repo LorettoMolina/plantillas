f6aea19f7d1a54f55cfd71dc92aa7b35d568bca0
LorettoMolina <238060610601@cetis61.edu.mx>
2025-10-16 10:45:45 -0500

    actualizacion

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -21,6 +21,36 @@ def maravillas():
 @app.route('/acerca')
 def acerca():
     return render_template('acerca.html')
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    error = None  
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        gender = request.form['gender']
+        
+        
+        if password != confirm_password:
+            error = "La contraseña no coincide"  
+        else:
+            print(f"Registrado: {username}, Correo: {email}, Contraseña: {password}, Género: {gender}")
+            return redirect(url_for('inicio'))  
+        
+    return render_template('registro.html', error=error) 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        print(f"Iniciado sesión: {username}")
+        return redirect(url_for('inicio')) 
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
